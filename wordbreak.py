b5c8ddea9b803a696a14ead97cc644fb772bc4c9
Hemanth02a <hemanthkumarmynam@gmail.com>
2025-09-24 11:34:11 +0530

    Add files via upload

---
+++ wordbreak.py
@@ -0,0 +1,9 @@
+s=lowercase(String(input("enter the string ")))
+print(s)
+wordDict=lowercase(list[String(input("enter the sub words : "))])
+print(wordDict)
+if s==(wordDict[0]+wordDict[1]):
+return s+wordDict[0]+wordDict[1]
+print("ture")
+else:
+print("false")
